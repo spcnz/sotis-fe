1836554f6ab5ff1f7299d739e556abac377b7561
specnazm <laketic.milena98@gmail.om>
2021-11-09 20:35:23 +0100

    fixed login error

--- src/store/reducers/AuthReducer.js
+++ src/store/reducers/AuthReducer.js
@@ -8,7 +8,8 @@ const authReducer = (state =
   }, action) => {
   switch (action.type) {
     case AUTH_USER:
-      return { isAuth: action.payload }
+
+      return { isAuth: action.payload, role: AuthService.getRole() }
     default:
       return state;
   }
